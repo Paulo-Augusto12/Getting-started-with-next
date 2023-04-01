2641ce861e9cac3c9393acc1cda19dd934b903b3
Paulo Augusto <paulo-augusto12@outlook.com>
2023-04-01 18:36:36 -0300

    feat: added profile image asset

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
-
+import Image from "next/image";
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
       <Link href={"/"}> Back to home</Link>
+      <Image src="/images/profile_image.jpeg" height={144} width={144} />
     </>
   );
 }
