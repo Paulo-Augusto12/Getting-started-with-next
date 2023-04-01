2b10a82cd9e4e5594e63339d4993477167619818
Paulo Augusto <paulo-augusto12@outlook.com>
2023-04-01 18:43:47 -0300

    feat: added metadata to the file. Added alt property to image component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <Link href={"/"}> Back to home</Link>
-      <Image src="/images/profile_image.jpeg" height={144} width={144} />
+      <Image
+        src="/images/profile_image.jpeg"
+        height={144}
+        width={144}
+        alt="Profile Image"
+      />
     </>
   );
 }
